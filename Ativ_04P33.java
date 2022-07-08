406d9a3c5f30d1d377aaf429ab51520d97b45321
Milena Manoel Fernandes <106815386+mfMilena@users.noreply.github.com>
2022-07-08 18:08:58 -0300

    Update and rename Ativ_06.java to Ativ_04P33.java

--- Ativ_04P33.java
+++ Ativ_04P33.java
@@ -1,10 +1,11 @@
 public class Ativ_06 {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        Float Horas, Salario;
+        Float Salario;
+        Int Hora;
         System.out.println(" Digite a quantidade de horas trabalhadas: ");
         Hora = ler.nextFloat();
-        Salario = (10.25 * Horas);
+        Salario = (10.25f * Horas);
         System.out.println("O valor do salario é: " + Salario);
         }
     }
