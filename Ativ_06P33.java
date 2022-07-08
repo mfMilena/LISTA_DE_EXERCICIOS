d467eb7095b417b1f1621c3a4468b532566627a8
Milena Manoel Fernandes <106815386+mfMilena@users.noreply.github.com>
2022-07-08 18:30:14 -0300

    Update Ativ_06P33.java

--- Ativ_06P33.java
+++ Ativ_06P33.java
@@ -1,1 +1,26 @@
+package EXERCÍCIOS.ATIV_06;
 
+import java.util.Scanner;
+public class Ativ_08 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        Float Valor, Conversao;
+        String Opc;
+        System.out.println(" Digite 1 para converter Libras e 10 para converter Quilos: ");
+        Opc = ler.nextInt();
+        System.out.println(" Digite uma valor para converter: ");
+        Valor = ler.nextFloat();
+        Conversao = (10.25 * Horas);
+        System.out.println("O valor do salario é: " + Salario);
+        if (Opc == 1)
+        {
+            Conversao = Valor * 2.205f;
+            SySystem.out.println("Valor convertido para Libras sera de:" + Conversao + "aproximadamente");
+        }else
+        if (opc == 10)
+        {
+            Conversao = Valor / 2.205f;
+            SySystem.out.println("Valor convertido para Quilos sera de:" + Conversao + "aproximadamente");  
+        }
+        }
+    }
