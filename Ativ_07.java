cdd47233019aa8c60a7761112e05dc68b46da03b
Milena Manoel Fernandes <106815386+mfMilena@users.noreply.github.com>
2022-07-08 17:53:26 -0300

    Create Ativ_07.java

---
+++ Ativ_07.java
@@ -0,0 +1,14 @@
+public class Ativ_07 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        Float Horas, Salario;
+        System.out.println(" Digite a quantidade de horas trabalhadas: ");
+        Hora = ler.nextFloat();
+        Salario = (10.25 * Horas);
+        System.out.println("O valor do salario é: " + Salario);
+        if (Salario < 50) 
+        {
+            SySystem.out.println("Atenção, dirija-se à direção do Hotel!");
+        }
+        }
+    }
