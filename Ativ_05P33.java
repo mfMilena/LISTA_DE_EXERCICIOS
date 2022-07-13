8e581a2c98fb11d27ab6f4656571f58ad0a5d372
Milena Manoel Fernandes <106815386+mfMilena@users.noreply.github.com>
2022-07-13 08:30:40 -0300

    Update Ativ_05P33.java

--- Ativ_05P33.java
+++ Ativ_05P33.java
@@ -2,17 +2,17 @@ package EXERCÍCIOS.ATIV_05;
 
 import java.util.Scanner;
 public class Ativ_07 {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
         Float Salario;
-        Int Horas;
+        int Horas;
         System.out.println(" Digite a quantidade de horas trabalhadas: ");
-        Hora = ler.nextInt();
+        Horas = ler.nextInt();
         Salario = (10.25f * Horas);
         System.out.println("O valor do salario é: " + Salario);
         if (Salario < 50f) 
         {
-            SySystem.out.println("Atenção, dirija-se à direção do Hotel!");
+            System.out.println("Atenção, dirija-se à direção do Hotel!");
         }
         }
     }
