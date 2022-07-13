7ad5f757e75fed5074fd4749bf7f32bbb0f8c451
Milena Manoel Fernandes <106815386+mfMilena@users.noreply.github.com>
2022-07-13 09:28:34 -0300

    Add files via upload

---
+++ Ativ_03.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ativ_03 {public static void main(String[] args) {
+    Scanner ler = new Scanner(System.in);
+    Float Valor, Conversao;
+    int Opc;
+    System.out.println(" Digite 1 para converter Libras e 10 para converter Quilos: ");
+    Opc = ler.nextInt();
+    System.out.println(" Digite uma valor para converter: ");
+    Valor = ler.nextFloat();
+    if (Opc == 1)
+    {
+        Conversao = Valor * 2.205f;
+        System.out.println("Valor convertido para Libras sera de:" + Conversao + "aproximadamente");
+    }else
+    if (Opc == 10)
+    {
+        Conversao = Valor / 2.205f;
+        System.out.println("Valor convertido para Quilos sera de: " + Conversao + " aproximadamente");  
+    }
+    }
+}
